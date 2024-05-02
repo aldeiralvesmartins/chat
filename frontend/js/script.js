2c64dfbf616e111a877c4540a736122b4a866e50
aldeir <95594532+aldeiralvesmartins@users.noreply.github.com>
2024-05-02 20:46:42 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://chat-4v9b.onrender.com")
+    websocket = new WebSocket("wss://chat-4v9b.onrender.com")
     websocket.onmessage = processMessage
 }
 
